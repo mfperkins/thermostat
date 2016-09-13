715bc9dcf4c3a28e49dd9bececb247f9b20237cd
Matthew Perkins <mfperkins@gmail.com>
2016-09-13 16:22:48 +0100

    delivering user stories

--- spec/thermoSpec.js
+++ spec/thermoSpec.js
@@ -1,14 +1,22 @@
 describe("Thermostat", function() {
   var thermostat;
   var thermostat2;
+  var thermostat3;
+  var thermostat4;
 
   beforeEach(function() {
     thermostat = new Thermostat();
     thermostat2 = new Thermostat(10);
+    thermostat3 = new Thermostat(25);
+    thermostat4 = new Thermostat(32);
   });
 
   it("starts at a default of 20 degrees", function() {
     expect(thermostat.currentTemp()).toEqual(20);
+  });
+
+  it("starts with power save mode on", function() {
+    expect(thermostat._powerSave).toEqual(true);
   });
 
   it('increases temp by 1 degree using the up button', function(){
@@ -24,6 +32,21 @@ describe("Thermostat", function() {
   it('cannot go below 10', function(){
     thermostat2.downButton()
     expect(thermostat2.currentTemp()).toEqual(10);
+  });
+
+  it('should got to a max of 25 if power save mode on', function() {
+    thermostat3.upButton()
+    expect(thermostat3.currentTemp()).toEqual(25);
+  });
+
+  it('should got to a max of 32 if power save mode off', function() {
+    thermostat4.upButton()
+    expect(thermostat4.currentTemp()).toEqual(32);
+  });
+
+  it('should turn power saving mode off', function() {
+    thermostat.powerSave()
+    expect(thermostat._powerSave).toEqual(false);
   });
 
 });
--- src/thermo.js
+++ src/thermo.js
@@ -4,21 +4,32 @@ const MAX = 32;
 
 function Thermostat(temp = 20) {
   this._currentTemp = temp;
+  this._powerSave = true;
 };
 
+Thermostat.prototype = {
+    currentTemp: function(){
+      return this._currentTemp;
+    },
 
-  Thermostat.prototype = {
-      currentTemp: function(){
-        return this._currentTemp;
-      },
-      
-      upButton: function(){
+    powerSave: function() {
+      this._powerSave === true ? this._powerSave = false : this._powerSave = true;
+      return this._powerSave;
+    },
+
+    upButton: function() {
+      console.log(this._powerSave)
+      if (this._powerSave === true && this.currentTemp() < MAXP) {
         this._currentTemp ++ ;
-      },
+      }
+      else if (this.currentTemp < MAX) {
+        this._currentTemp ++ ;
+      };
+    },
 
-      downButton: function() {
-        if (this._currentTemp > MIN){
-        this._currentTemp -- };
-      },
+    downButton: function() {
+      if (this._currentTemp > MIN){
+      this._currentTemp -- };
+    },
 
-  };
+};
